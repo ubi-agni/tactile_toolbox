c4df3fea83a75e9e6590bdadf93e25d368ddcafa
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2016-03-09 02:14:44 +0100

    better variable naming

--- src/tactile_state_publisher.cpp
+++ src/tactile_state_publisher.cpp
@@ -104,12 +104,12 @@ void TactileStatePublisher::tactile_state_cb(const tactile_msgs::TactileStateCon
     if (it == sensor_data_map_.end()) continue;
 
     // store new data in the tactile_msg
-    const sensor_msgs::ChannelFloat32::_values_type &values = msg->sensors[i].values;
-    sensor_msgs::ChannelFloat32::_values_type &dest_values = tactile_msg_.sensors[it->second].values;
-    if (dest_values.size() == values.size())
+    const sensor_msgs::ChannelFloat32::_values_type &src = msg->sensors[i].values;
+    sensor_msgs::ChannelFloat32::_values_type &dst = tactile_msg_.sensors[it->second].values;
+    if (dst.size() == src.size())
     {
       boost::unique_lock<boost::shared_mutex> lock(mutex_);
-      std::copy(values.begin(), values.end(), dest_values.begin());
+      std::copy(src.begin(), src.end(), dst.begin());
     }
   }
 }
