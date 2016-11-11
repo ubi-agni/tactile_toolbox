464c49ff82f1f257be8de27bdf3820fdcdbb9d93
Dennis Stoppel <dstoppel@techfak.uni-bielefeld.de>
2016-11-11 10:04:57 +0100

    invert loop-structure, add bumper for testing cleaning up comments

--- gazebo_plugins/gazebo_ros_tactile/src/gazebo_ros_tactile.cpp
+++ gazebo_plugins/gazebo_ros_tactile/src/gazebo_ros_tactile.cpp
@@ -37,12 +37,14 @@
 #include <gazebo/math/Vector3.hh>
 
 #include <tf/tf.h>
-
+#include <sensor_msgs/ChannelFloat32.h>
 #include <gazebo_ros_tactile/gazebo_ros_tactile.h>
 #include <gazebo_plugins/gazebo_ros_utils.h>
 
 #include <urdf_tactile/tactile.h>
 #include <urdf/sensor.h>
+//#include <ros/console.h>
+//#include <urdf_tactile/taxel_info_iterator.h>
 #include <urdf_tactile/cast.h>
 
 
@@ -56,9 +58,7 @@ GZ_REGISTER_SENSOR_PLUGIN(GazeboRosTactile)
 // Constructor
 GazeboRosTactile::GazeboRosTactile() : SensorPlugin()
 {
-	//std Vector befüllen mit gz Vector3, der alleCenter und Normals enthält
-	//sinnlos? Positionen können sich ändern
-	//parsen im Constructor? Sprich Vektor mit Pointern? Sensoren oder Texels?
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -98,9 +98,13 @@ void GazeboRosTactile::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
   // "publishing contact/collisions to this topic name: "
   //   << this->bumper_topic_name_ << std::endl;
   this->bumper_topic_name_ = "bumper_states";
+  this->tactile_topic_name_ = "tactile_states";
   if (_sdf->HasElement("bumperTopicName"))
     this->bumper_topic_name_ =
       _sdf->GetElement("bumperTopicName")->Get<std::string>();
+  if (_sdf->HasElement("tactileTopicName"))
+    this->tactile_topic_name_ =
+      _sdf->GetElement("tactileTopicName")->Get<std::string>();
 
   // "transform contact/collisions pose, forces to this body (link) name: "
   //   << this->frame_name_ << std::endl;
@@ -154,20 +158,19 @@ void GazeboRosTactile::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
   this->rosnode_->getParam(std::string("tf_prefix"), prefix);
   this->frame_name_ = tf::resolve(prefix, this->frame_name_);
   
-  //TODO Parse here
-  // class TactileSensor : urdf::SensorBase
-  
+
+//Begin parsing  
+  ROS_INFO_STREAM_ONCE("bumper_topic_name_" << bumper_topic_name_);
   this-> sensors = urdf::parseSensorsFromParam("robot_description", urdf::getSensorParser("tactile"));
-  this-> numOfSensors=sensors.size();
+  
    
   
   
-  this->taxelPositions.clear();//resize(numOfSensors);//, std::vector<gazebo::math::Vector3>(numOfTaxels, gazebo::math::Vector3(0, 0, 0)));
-  this->taxelNormals.clear();//resize(numOfSensors);//, std::vector<gazebo::math::Vector3>(numOfTaxels, gazebo::math::Vector3(0, 0, 0)));
+  this->taxelPositions.clear();
+  this->taxelNormals.clear();
   
-  //this->numOfTaxels.resize(numOfSensors,12);
   
-  unsigned int i=0;
+  this-> numOfSensors=0;
   for (auto it = sensors.begin(), end = sensors.end(); it != end; ++it) {
 		urdf::tactile::TactileSensorConstSharedPtr sensor = urdf::tactile::tactile_sensor_cast(it->second);
 		if (!sensor) continue;  // some other sensor than tactile
@@ -175,45 +178,39 @@ void GazeboRosTactile::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
 		
 		
 		this->numOfTaxels.push_back(sensor -> taxels_.size());
-		this->taxelNormals.push_back(std::vector<gazebo::math::Vector3>(numOfTaxels[i], gazebo::math::Vector3(0, 0, 0)));//[i].resize(numOfTaxels[i]);
-		this->taxelPositions.push_back(std::vector<gazebo::math::Vector3>(numOfTaxels[i], gazebo::math::Vector3(0, 0, 0)));//[i].resize(numOfTaxels[i]);
-		for(unsigned int j=0; j < numOfTaxels[i]; j++){
-			this->taxelPositions[i][j]=
+		this->taxelNormals.push_back(std::vector<gazebo::math::Vector3>(numOfTaxels[this-> numOfSensors], gazebo::math::Vector3(0, 0, 0)));//[i].resize(numOfTaxels[i]);
+		this->taxelPositions.push_back(std::vector<gazebo::math::Vector3>(numOfTaxels[this-> numOfSensors], gazebo::math::Vector3(0, 0, 0)));//[i].resize(numOfTaxels[i]);
+		sensor_msgs::ChannelFloat32 channel;
+		channel.values.resize(numOfTaxels[this-> numOfSensors]);
+		this->tactile_state_msg_.sensors.push_back(channel);
+		for(unsigned int j=0; j < numOfTaxels[this-> numOfSensors]; j++){
+			this->taxelPositions[this-> numOfSensors][j]=
 				gazebo::math::Vector3(	sensor->taxels_[j] -> origin.position.x,
 										sensor->taxels_[j] -> origin.position.y,
 										sensor->taxels_[j] -> origin.position.z);
-										
-			this->taxelNormals[i][j]=gazebo::math::Vector3(0, i, j);
-			//normal=rotation times zAxis?
+			
+			//normal=rotation times zAxis
+			urdf::Vector3 zAxis(0,0,1);							
+			urdf::Vector3 urdfTaxelNormal=(sensor->taxels_[j] -> origin.rotation)*zAxis;
+			this->taxelNormals[this-> numOfSensors][j]=gazebo::math::Vector3(	urdfTaxelNormal.x,
+																				urdfTaxelNormal.y,
+																				urdfTaxelNormal.z);
+
 		}
-		i++;
-		//else mitzählen, wahre Sensornumber
-
+		this->tactile_state_msg_.sensors[numOfSensors].name="Sensor_" + std::to_string(numOfSensors);// >> numOfSensors;
+		this-> numOfSensors++;
 	}
-  this-> numOfSensors=i;
-  this->tactile_state_msg_.sensors.resize(numOfSensors);
-  /*
-  for(unsigned int i=0; i < numOfSensors; i++){
-	  //this->numOfTaxels[i]=12;//sensors[i].array_ -> rows;
-	  ROS_INFO_STREAM_ONCE("numOfTaxels" << i << " :" <<numOfTaxels[i]);
-	  this->tactile_state_msg_.sensors[i].values.resize(numOfTaxels[i]);
-	  this->taxelPositions[i].resize(numOfTaxels[i]);
-	  this->taxelNormals[i].resize(numOfTaxels[i]);
-	  // slso resize taxelpos and taxel normal
-	  for(unsigned int j=0; j < numOfTaxels[i]; j++){
-		  this->taxelNormals[i][j]=gazebo::math::Vector3(i, j, 0);
-		  this->taxelPositions[i][j]=gazebo::math::Vector3(0, i, j);
-		  
-	  }
-	  
-  }*/
 
-/*
+	  
+
+
   this->contact_pub_ = this->rosnode_->advertise<gazebo_msgs::ContactsState>(
-    std::string(this->bumper_topic_name_), 1);*/
+    std::string(this->bumper_topic_name_), 1);
     
   this->tactile_pub_ = this->rosnode_->advertise<tactile_msgs::TactileState>(
-    std::string(this->bumper_topic_name_), 1);
+    std::string(this->tactile_topic_name_), 1);
+    
+
 
   // Initialize
   // start custom queue for contact bumper
@@ -233,7 +230,7 @@ void GazeboRosTactile::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
 // Update the controller
 void GazeboRosTactile::OnContact()
 {
-  if (this->tactile_pub_.getNumSubscribers() <=0) //contact_pub_.getNumSubscribers() <= 0)
+  if (this->tactile_pub_.getNumSubscribers() <=0 && contact_pub_.getNumSubscribers() <= 0)
     return;
 
   msgs::Contacts contacts;
@@ -242,9 +239,8 @@ void GazeboRosTactile::OnContact()
   this->tactile_state_msg_.header.frame_id = this->frame_name_;
   common::Time meastime = this->parentSensor->GetLastMeasurementTime();
   this->tactile_state_msg_.header.stamp = ros::Time(meastime.sec, meastime.nsec);
-  //this->contact_state_msg_.header.stamp = ros::Time(contacts.time().sec(),
-  //                             contacts.time().nsec());
-  //TODO this->tactile_state_msg_.header.seq=?
+  this->contact_state_msg_.header.frame_id = this->frame_name_;
+  this->contact_state_msg_.header.stamp = ros::Time(meastime.sec, meastime.nsec);
 
   // get reference frame (body(link)) pose and subtract from it to get
   // relative force, torque, position and normal vectors
@@ -271,36 +267,37 @@ void GazeboRosTactile::OnContact()
   }
 
 
-  ROS_INFO_STREAM_THROTTLE(1.0, "frame_pos" << frame_pos.x << "frame_rot" << frame_rot.x);
+  //ROS_INFO_STREAM_THROTTLE(1.0, "frame_pos" << frame_pos.x << "frame_rot" << frame_rot.x);
 
   // set contact states size
   this->contact_state_msg_.states.clear();
   
-  //this->tactile_state_msg_.sensors.clear(); //is this the right way? //std::vector assign or close, to put zeros for values, now: implicitly done
+ 
+  
 
   
-  //tactile::urdf::SensorMap sensors;
-for (unsigned int m=0; m < this->numOfSensors; m++){	//Loop over Sensors
-  for (unsigned int k =0; k < this->numOfTaxels[m]; k++) //Loop over taxels //taxelpos.size();k++)
-  {
-  
-    sensor_msgs::ChannelFloat32 &tSensor = this->tactile_state_msg_.sensors[m];
-	 
-	 
-	 
-	 float finalProjectedForce=0.0f; //not necessary here
-	  
-	  
-	  
-	  // Loop over Collisions
-	  // GetContacts returns all contacts on the collision body
-	  unsigned int contactsPacketSize = contacts.contact_size();
-	  for (unsigned int i = 0; i < contactsPacketSize; ++i)
-	  {
-		  // Create a ContactState
-		  //gazebo_msgs::ContactState state;
+unsigned int contactsPacketSize = contacts.contact_size();
+//Deklarationen
+		  unsigned int contactGroupSize;
+      //ROS_INFO_STREAM_THROTTLE(1.0, "contactGroupSize" << contactGroupSize);
+		  double normalForceScalar;
+		  double stdDev=2.0;
+		  double distance;
+		  double critDist=1.0;
+		  
+		  double p=1.0;	//Multiplicator
+		  const double pi= 3.14159265359;
+		  double minForce=0.1;
+      
+      float finalProjectedForce;
+      
+	  for (unsigned int i = 0; i < contactsPacketSize; ++i)  {
+      // Create a ContactState
+      gazebo_msgs::ContactState state;
+      
 		  gazebo::msgs::Contact contact = contacts.contact(i);
-		  /*
+      
+    
 		  state.collision1_name = contact.collision1();
 		  state.collision2_name = contact.collision2();
 		  std::ostringstream stream;
@@ -311,15 +308,16 @@ for (unsigned int m=0; m < this->numOfSensors; m++){	//Loop over Sensors
 		  << std::endl;
 		  state.info = stream.str();
 		  
+      
+      
 		  state.wrenches.clear();
 		  state.contact_positions.clear();
 		  state.contact_normals.clear();
 		  state.depths.clear();
 		  
-		  //create a tactile state
-		  tactile_msgs::TactileState tState;
+
 		  
-		  //sensor_msgs::ChannelFloat32 tSensors;##
+
 
 		  
 		  // sum up all wrenches for each DOF
@@ -330,29 +328,18 @@ for (unsigned int m=0; m < this->numOfSensors; m++){	//Loop over Sensors
 		  total_wrench.torque.x = 0;
 		  total_wrench.torque.y = 0;
 		  total_wrench.torque.z = 0;
-		  */
-		  
-		  //Deklarationen
-		  unsigned int contactGroupSize = contact.position_size();
-		  double normalForceScalar;
-		  double stdDev=2.0;
-		  double distance;
-		  double critDist=1.0;
-		  //double finalProjectedForce=0.0;
-		  double p=1.0;	//Multiplicator
-		  const double pi= 3.14159265359;
-		  
-		  
+  
+      
+		  contactGroupSize = contact.position_size();
 		  for (unsigned int j = 0; j < contactGroupSize; ++j)
 		  {
-		  
-		  
+ 
+ 		  
 		  //math::Pose frame_pose;
 		  //frame_pos = math::Vector3(0, 0, 0);
 		  //
-		  ROS_INFO_STREAM_THROTTLE(1.0,"state.contact_positions.x:" << frame_pos.x);
-		  //double distance= sqrt((frame_pos.x-state.contact_positions.x)*(frame_pos.x-state.contact_positions.x));//sqrt((frame_pos.x-state.contact_positions.x)*(frame_pos.x-state.contact_positions.x)+(frame_pos.y-state.contact_positions.y)*(frame_pos.y-state.contact_positions.y)+(frame_pos.z-state.contact_positions.z)*(frame_pos.z-state.contact_positions.z)); 
-		  
+		  //ROS_INFO_STREAM_THROTTLE(1.0,"state.contact_positions.x:" << frame_pos.x); 
+
 		  // Get force, torque and rotate into user specified frame.
 		  // frame_rot is identity if world is used (default for now)
 		  math::Vector3 force = frame_rot.RotateVectorReverse(math::Vector3(
@@ -363,51 +350,73 @@ for (unsigned int m=0; m < this->numOfSensors; m++){	//Loop over Sensors
 		  contact.wrench(j).body_1_wrench().torque().x(),
 		  contact.wrench(j).body_1_wrench().torque().y(),
 		  contact.wrench(j).body_1_wrench().torque().z()));
-		  
-		  
-		  
+ 
+       // set wrenches
+      geometry_msgs::Wrench wrench;
+      wrench.force.x  = force.x;
+      wrench.force.y  = force.y;
+      wrench.force.z  = force.z;
+      wrench.torque.x = torque.x;
+      wrench.torque.y = torque.y;
+      wrench.torque.z = torque.z;
+      state.wrenches.push_back(wrench);
+
+      total_wrench.force.x  += wrench.force.x;
+      total_wrench.force.y  += wrench.force.y;
+      total_wrench.force.z  += wrench.force.z;
+      total_wrench.torque.x += wrench.torque.x;
+      total_wrench.torque.y += wrench.torque.y;
+      total_wrench.torque.z += wrench.torque.z;
+
+
 		  //////////////////////////BEGIN OF FORCE TRANSFORMTION
-		  // transform contact positions into relative frame
-		  // set contact positions
-		  gazebo::math::Vector3 position = frame_rot.RotateVectorReverse(
-		  math::Vector3(contact.position(j).x(),
-		  contact.position(j).y(),
-		  contact.position(j).z()) - frame_pos);
-		  geometry_msgs::Vector3 contact_position;
-		  contact_position.x = position.x;
-		  contact_position.y = position.y;
-		  contact_position.z = position.z;
-		  //state.contact_positions.push_back(contact_position);
+      // transform contact positions into relative frame
+      // set contact positions
+      gazebo::math::Vector3 position = frame_rot.RotateVectorReverse(
+          math::Vector3(contact.position(j).x(),
+                        contact.position(j).y(),
+                        contact.position(j).z()) - frame_pos);
+      geometry_msgs::Vector3 contact_position;
+      contact_position.x = position.x;
+      contact_position.y = position.y;
+      contact_position.z = position.z;
+      state.contact_positions.push_back(contact_position);
+
+      // rotate normal into user specified frame.
+      // frame_rot is identity if world is used.
+      math::Vector3 normal = frame_rot.RotateVectorReverse(
+          math::Vector3(contact.normal(j).x(),
+                        contact.normal(j).y(),
+                        contact.normal(j).z()));
+      // set contact normals
+      geometry_msgs::Vector3 contact_normal;
+      contact_normal.x = normal.x;
+      contact_normal.y = normal.y;
+      contact_normal.z = normal.z;
+      state.contact_normals.push_back(contact_normal);
+
+      // set contact depth, interpenetration
+      state.depths.push_back(contact.depth(j));
 		  
-		  // rotate normal into user specified frame.
-		  // frame_rot is identity if world is used.
-		  math::Vector3 normal = frame_rot.RotateVectorReverse(
-		  math::Vector3(contact.normal(j).x(),
-		  contact.normal(j).y(),
-		  contact.normal(j).z()));
-		  // set contact normals
-		  geometry_msgs::Vector3 contact_normal;
-		  contact_normal.x = normal.x;
-		  contact_normal.y = normal.y;
-		  contact_normal.z = normal.z;
-		  //state.contact_normals.push_back(contact_normal);
-		  
+
 		  ////////////////////////////////////END OF FORCE TRANSFORMATION
-		  
-		  
-		  
-		  
-		  //TEST Dotproduct
-		  //distance=math::Vector3::Dot(transformedForceAP, transformedForceAP)const;
-		  
-		  
-		  //Distanzbestimmen zwischen Kraft und Taxelzentrum
+      
+for (unsigned int m=0; m < this->numOfSensors; m++){	//Loop over Sensors
+  for (unsigned int k =0; k < this->numOfTaxels[m]; k++) //Loop over taxels 
+  {
+  
+    //sensor_msgs::ChannelFloat32 &tSensor = this->tactile_state_msg_.sensors[m];
+
+	 finalProjectedForce=0.0f; //not necessary here
+
+		  //calc distance between force-ap and taxelcenter
 		  distance=sqrt(pow((position.x-taxelPositions[m][k].x),2)+pow((position.y-taxelPositions[m][k].y),2)+pow((position.z-taxelPositions[m][k].z),2));
 
-		  if(distance>critDist){
-			finalProjectedForce+=0.0f; // bzw continue;
+		  if(distance > critDist){
+			finalProjectedForce=0.0f; // bzw continue;
 			//nächster Durchlauf
 		  }
+      
 		  else{
 			//project Force on normal
 			normalForceScalar=(this->taxelNormals[m][k].x*force.x+this->taxelNormals[m][k].y*force.y+this->taxelNormals[m][k].z*force.z)/sqrt(pow(this->taxelNormals[m][k].x,2)+pow(this->taxelNormals[m][k].y,2)+pow(this->taxelNormals[m][k].z,2)); //Normalize the taxelNormals, lookup if it norm.
@@ -415,36 +424,39 @@ for (unsigned int m=0; m < this->numOfSensors; m++){	//Loop over Sensors
 			if(normalForceScalar > 0){
 				//Normalverteilung erzeugen
 				p=exp(-(distance*distance/(2*stdDev*stdDev)))/sqrt(2*pi*stdDev*stdDev);
-				finalProjectedForce+=p*normalForceScalar;
+				finalProjectedForce=p*normalForceScalar;
 			}
-		  
 		  }
-		  
-		  
-		  
-		  
-		  
-		  // set contact depth, interpenetration
-		  //state.depths.push_back(contact.depth(j));
-		  }//END FOR contactGroupSize
-		  
-		  
-		  //state.total_wrench = total_wrench;
-		  //this->contact_state_msg_.states.push_back(state);
-		  
-	  }// END FOR contactsPacketSize (What does this mean?)
-	  //is the force to small first contact thershold
-	  if(finalProjectedForce < forceSensivity){
-		  finalProjectedForce=0.0f;
-	  }
-	  
-	  
-	  this->tactile_state_msg_.sensors[m].values[k]=finalProjectedForce;//tSensor.values.push_back(finalProjectedForce); //if changed above, access, not pubsh_back
-  }//END FOR Taxels
-  this->tactile_pub_.publish(this->tactile_state_msg_);
-}//END FOR Sensors
+      
+		  this->tactile_state_msg_.sensors[m].values[k]+=finalProjectedForce;
+
+		  }//END FOR Taxels
+	  }// END FOR Sensors
+	   }//END FOR contactGroupSize
   
-  }
+    state.total_wrench = total_wrench;
+    this->contact_state_msg_.states.push_back(state);
+    
+  }//END FOR contactsPacketSize 
+ 
+for(unsigned int e=0; e < this->numOfSensors; e++){
+	
+	for (unsigned int f=0; f< this->numOfTaxels[e]; f++){
+		if (this->tactile_state_msg_.sensors[e].values[f] < minForce){
+			this->tactile_state_msg_.sensors[e].values[f]=0;
+		}
+	}
+}
+  this->contact_pub_.publish(this->contact_state_msg_);
+  this->tactile_pub_.publish(this->tactile_state_msg_);
+  
+  for(unsigned int e=0; e < this->numOfSensors; e++){
+      for (unsigned int f=0; f < this->numOfTaxels[e]; f++){
+        this->tactile_state_msg_.sensors[e].values[f]=0.0;
+      }
+      
+    }
+}
   
   
   ////////////////////////////////////////////////////////////////////////////////
