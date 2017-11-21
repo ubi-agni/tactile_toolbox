e8acab6cdf405800e9add58f999859806c0c8900
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2017-11-21 18:46:25 +0100

    Fixed display timeout computation to be completely based on Now and not on sensor stamp (permits playing data recorded in the past)

--- rviz_tactile_plugins/src/tactile_state_display.cpp
+++ rviz_tactile_plugins/src/tactile_state_display.cpp
@@ -325,7 +325,7 @@ void TactileStateDisplay::processMessage(const tactile_msgs::TactileState::Const
     const std::string &channel = sensor->name;
     auto range = sensors_.equal_range(channel);
     for (auto s = range.first, range_end = range.second; s != range_end; ++s) {
-      s->second->update(msg->header.stamp, sensor->values);
+      s->second->update(ros::Time::now(), sensor->values);
     }
   }
 }
