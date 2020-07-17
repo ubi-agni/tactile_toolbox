e2879fa1dd487bcc0f7a187af0771289423c628b
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2020-07-17 20:59:14 +0200

    Added support for extending existing mapping

--- tactile_calibration/tactile_calibration_generator/scripts/generate_calib.py
+++ tactile_calibration/tactile_calibration_generator/scripts/generate_calib.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 import csv
 import yaml
 import time
+import os
 
 #pip install pwlf
 import pwlf
@@ -19,6 +20,7 @@ REF_FLAT_THRESHOLD = 0.3 # in newton (30 gram) accepatable variation to find the
 DEFAULT_INPUT_RESOL = 12 # in bits
 CHANGE_DETECT_THRESH = 4 # 5 unit of velocity positive or negative
 DEFAULT_SEGMENTS = 5
+DEFAULT_MAPPING_FILENAME = "mapping.yaml"
 
 
 def smooth(y, box_pts):
@@ -73,12 +75,70 @@ def find_inflection_index(x):
     else:  # inverse decreasing and increasing to have always "pushing actions" first
         return [np.array(decreasing_idx), np.array(increasing_idx)]
 
+def check_idx_range_duplicate(idx_ranges, data_channel):
+    new_idx_ranges = []
+    found_duplicate = False
+    for idx_range in idx_ranges:
+        #print "  found range", idx_range
+        if not found_duplicate:
+            if type(idx_range) == list:
+                if data_channel >  idx_range[0] and data_channel < idx_range[1]:  # stricly within the range
+                    if data_channel-1 == idx_range[0]: # touches one extremity, this extremity is alone
+                        new_idx_ranges.append(idx_range[0])
+                    if data_channel+1 == idx_range[1]: # touches other extremity, this extremity is alone
+                        # no gap, only 2 extremeties
+                        new_idx_ranges.append(idx_range[1])
+                    if data_channel-1 > idx_range[0]:
+                        new_idx_ranges.append([idx_range[0], data_channel-1])
+                    if data_channel+1 < idx_range[1]:
+                        new_idx_ranges.append([data_channel+1, idx_range[1]])                                      
+                    found_duplicate = True
+                else: # within the range but on the extremities or out of the range
+                    if data_channel == idx_range[0] or data_channel == idx_range[1]: # is on one extremity
+                        if data_channel == idx_range[0]: # one extremity
+                            if data_channel+1 == idx_range[1]: # end is single
+                                new_idx_ranges.append(idx_range[1])
+                            else: # end is not single
+                                new_idx_ranges.append([data_channel+1, idx_range[1]])
+                        if data_channel == idx_range[1]: # other extremity
+                            if data_channel-1 ==  idx_range[0]: # end is single
+                                new_idx_ranges.append(idx_range[0])
+                            else:
+                                new_idx_ranges.append([idx_range[0], data_channel-1])
+                        found_duplicate = True
+                    #else:  out of the range 
+                # continue
+            else: #int
+                if idx_range == data_channel:
+                    found_duplicate = True
+                    # don't append, since this data_channel will have its own calib
+        else: # concatenate the remaining ranges
+            new_idx_ranges.append(idx_range)
+    if found_duplicate:
+        #print "found duplicate "
+        return new_idx_ranges
+    else:
+        return None
+
+
 def is_raw(data):
     # check if there data are pure integers
     if np.sum(data.astype(int) - data) ==0 :
         return True
     return False
 
+# handle yaml export
+
+class flowmap( list ): pass
+def flowmap_rep(dumper, data):
+    return dumper.represent_sequence( u'tag:yaml.org,2002:seq', data, flow_style=True )
+
+# class blockseq( dict ): pass
+# def blockseq_rep(dumper, data):
+    # return dumper.represent_mapping( u'tag:yaml.org,2002:map', data, flow_style=False )
+
+# yaml.add_representer(blockseq, blockseq_rep)
+yaml.add_representer(flowmap, flowmap_rep)
 
 if __name__ == "__main__":
     # execute only if run as a script
@@ -89,12 +149,16 @@ if __name__ == "__main__":
                       help="bag filename to open")
     parser.add_argument("topic", type=str,
                       help="topic to process")
+    parser.add_argument("--mapping_file", type=str,
+                      help="output mapping filename, default is mapping.yaml")
     parser.add_argument("--no_extrapolation", action="store_true",
                       help="deactivate extrapolation of the mapping at both ends (O and input_range-1)")
     parser.add_argument("--data_channel", type=int,
                       help="index of the data channel to calibrate")
     parser.add_argument("--ref_channel", type=int,
                       help="index of the ref channel to calibrate against")
+    parser.add_argument("--ref_is_raw",  action="store_true",
+                      help="activate if the ref channel should be calibrated, and provide ref_ratio & ref_offset")
     parser.add_argument("--ref_tare_val", type=float,
                       help="use provided reference tare value (given in newton) instead of using rest pose values")
     parser.add_argument("--ref_ratio", type=float, default=REF_CALIB_RATIO,
@@ -252,7 +316,6 @@ if __name__ == "__main__":
     lookup_inc = OrderedDict()
     lookup_dec = OrderedDict()
     for val in range(input_range_max):
-
         # Get the index of increasing elements with value val   3.d1        Seek Lookup(n=0) within RAW-Taxel-values
         #print "inc_idx ", inc_idx
         idx_of_val_inc = np.where(raw[inc_idx] == val)
@@ -352,12 +415,74 @@ if __name__ == "__main__":
     # a    Save Lookuptable and-or Model in TaxelCalibrationMapping file.
     #yaml.add_representer(OrderedDict, lambda dumper, data: dumper.represent_mapping('tag:yaml.org,2002:map', data.items()))
     #yaml.add_representer(tuple, lambda dumper, data: dumper.represent_sequence('tag:yaml.org,2002:seq', data))
+    # handle previous file
+    if args.mapping_file:
+        mapping_filename = args.mapping_file
+    else:
+        mapping_filename = DEFAULT_MAPPING_FILENAME
+    
     represent_dict_order = lambda self, data:  self.represent_mapping('tag:yaml.org,2002:map', data.items())
     yaml.add_representer(OrderedDict, represent_dict_order)  
+    # check if mapping file exists
+    rename_oldcalib = False
+    calib = None
+    newcalib = {"calib": []}
+    try:
+        # read previous data if exists
+        with open(mapping_filename, 'r') as f:
+            # parse yaml
+            previous_calib = yaml.load(f)
+            if previous_calib is not None:
+                # check if previous calib is single calib format or not
+                if "calib" in previous_calib:
+                    # multi calib file
+                    calib = previous_calib
+                else:
+                    # old calib file
+                    # double check there are only digits as keys
+                    if previous_calib:
+                        for k in previous_calib.keys():
+                            if type(k) != int:
+                                print "file is not a correct mapping, and cannot be merged"
+                                rename_oldcalib = True
+                        if not rename_oldcalib:
+                            print "Old calib file detected, converting to new format and merging"
+                            calib = {"calib": [{'sensor_name': sensor_name, 'type': 'PWL', 'idx_range': flowmap([-1]), "values": previous_calib}]}
+            #else: # file is empty
+                
+        if rename_oldcalib is True:
+            # TODO rename
+            os.rename(mapping_filename, mapping_filename + ".bak")
+        # merge calib if needed
+        if calib is None: # no merge possible, or file empty then just write
+            newcalib["calib"] = [{'sensor_name': sensor_name, 'type': 'PWL', 'idx_range':  flowmap([calib_channel]), "values": mapping_dict}]
+        else: # merge needed
+            # TODO insert the new channel at the correct place
+            # TODO maybe allow to insert this calib for more than calib_channel
+            # check if duplicate data_channel
+            for cal in calib["calib"]:
+                new_idx_range = check_idx_range_duplicate(cal["idx_range"], calib_channel)
+                if new_idx_range is not None: # data must be split
+                    print " found a calibration for channel", calib_channel, " replacing with new calib"
+                    if len(new_idx_range):
+                        # keep the others
+                        if type(new_idx_range)==list:
+                            print new_idx_range
+                            newcalib["calib"].append({'sensor_name': cal["sensor_name"], 'type': cal["type"], 'idx_range':  flowmap(new_idx_range) , "values": cal["values"]})
+                        else:
+                            newcalib["calib"].append({'sensor_name': cal["sensor_name"], 'type': cal["type"], 'idx_range': flowmap([new_idx_range]), "values": cal["values"]})
+                else: # just append
+                    newcalib["calib"].append({'sensor_name': cal["sensor_name"], 'type': cal["type"], 'idx_range':  flowmap(cal["idx_range"]) , "values": cal["values"]})
+            # append new calib
+            newcalib["calib"].append({'sensor_name': sensor_name, 'type': 'PWL', 'idx_range': flowmap([calib_channel]), "values": mapping_dict})
 
-    calib = {"calib": [{'sensor_name': sensor_name, 'type': 'PWL', 'idx_range': [args.data_channel], "values": mapping_dict}]}
-    with open('mapping.yaml', 'w') as f:
-        data = yaml.dump(calib, f)  #sort_keys=False in python3 only     
+    except IOError:
+        # file does not exist, create it
+        newcalib["calib"] = [{'sensor_name': sensor_name, 'type': 'PWL', 'idx_range': flowmap([calib_channel]), "values": mapping_dict}]
+        pass
+    print "Saving mapping to ", mapping_filename
+    with open(mapping_filename, 'w') as f:
+        data = yaml.dump(newcalib, f, default_flow_style=False)  #sort_keys=False in python3 only
 
     
 
