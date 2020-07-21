c13a1b6fcceb60a164fc322782073a4ce3626b5e
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2020-07-21 13:54:47 +0200

    Fixed plot always true

--- tactile_calibration/tactile_calibration_tools/scripts/record_calib.py
+++ tactile_calibration/tactile_calibration_tools/scripts/record_calib.py
@@ -510,7 +510,7 @@ if __name__ == "__main__":
             ## basic data size check
             # TODO
             ## push/release check and plot the last recording
-            [inc, dec] = get_push_release_from_msgs(msgs, CHANGE_DETECT_THRESH, True)
+            [inc, dec] = get_push_release_from_msgs(msgs, CHANGE_DETECT_THRESH, args.plot)
             
             if inc is None:
                 print " failed to extract push/release, verify the data visually, do you want to save anyway ?"
