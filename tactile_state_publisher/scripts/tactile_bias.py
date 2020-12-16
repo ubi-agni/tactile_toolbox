daaa362fa0fd286f97e314ba890320d9228ac90a
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2020-12-16 17:20:12 +0100

    Improved user interface and permitted sample size change from cmdline

--- tactile_state_publisher/scripts/tactile_bias.py
+++ tactile_state_publisher/scripts/tactile_bias.py
@@ -2,8 +2,15 @@
 # license removed for brevity
 
 import rospy
-from tactile_state_publisher.tactile_bias_module import tactile_bias
+from tactile_state_publisher.tactile_bias_module import tactile_bias, SAMPLE_SIZE
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Applies a bias on each value of a TactileState \
+                                     message incoming on /tactile_states and outgoing on /tactile_states_biased.\n\
+                                     \n Bias is computed at start or on request through /tactile_bias/bias service')
+    parser.add_argument("samples", type=int, nargs='?', default=SAMPLE_SIZE,
+                        help="sample size for average computation of the bias, default is " + str(SAMPLE_SIZE))
+    args = parser.parse_args()
     rospy.init_node('tactile_bias', anonymous=False)
-    tactile_bias()
+    tactile_bias(args.samples)
