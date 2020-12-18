daa660dcfabf322862ec0f43d2ef2b0e362b7fab
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2020-12-18 11:27:31 +0100

    Fixed parser to allow topic remapping

--- tactile_state_publisher/scripts/tactile_bias.py
+++ tactile_state_publisher/scripts/tactile_bias.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Applies a bias on each value of a TactileState \
                                      message incoming on /tactile_states and outgoing on /tactile_states_biased.\n\
                                      \n Bias is computed at start or on request through /tactile_bias/reset service')
-    parser.add_argument("samples", type=int, nargs='?', default=SAMPLE_SIZE,
+    parser.add_argument("--samples", type=int, nargs='?', default=SAMPLE_SIZE,
                         help="sample size for average computation of the bias, default is " + str(SAMPLE_SIZE))
-    args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
     rospy.init_node('tactile_bias', anonymous=False)
     tactile_bias(args.samples)
