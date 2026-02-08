ec0c7c76a4e19a300448b92dccdf001f93c2834a
Robert Haschke <rhaschke@users.noreply.github.com>
2026-02-08 05:44:11 -0500

    Rename tactile_merger -> contact_force_estimator (#28)

--- contact_force_estimator/src/taxel.cpp
+++ contact_force_estimator/src/taxel.cpp
@@ -26,7 +26,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <tactile_merger/taxel.h>
+#include <contact_force_estimator/taxel.h>
 
 namespace tactile {
 
